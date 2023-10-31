478a77ed1a825500f5332e70f79a459e5c108756
Shaz <k.shaznazar@gmail.com>
2023-10-31 12:50:07 +0530

    Added functions for loading, training and evaluation

---
+++ Notebooks/some_load_train_evaluate_functions.py
@@ -0,0 +1,553 @@
+# Import libraries
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from dotenv import load_dotenv
+import os
+
+from neo4j import GraphDatabase
+
+from sklearn.model_selection import train_test_split
+
+from sklearn.impute import SimpleImputer
+from sklearn.impute import KNNImputer
+from fancyimpute import IterativeImputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+from sklearn.metrics import confusion_matrix, classification_report, roc_curve, auc
+
+from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.feature_selection import RFE
+from sklearn.feature_selection import SelectFromModel
+from sklearn.feature_selection import RFECV
+from skrebate import ReliefF
+from collections import Counter
+
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
+import optuna
+
+from sklearn.model_selection import StratifiedKFold
+
+from IPython.display import clear_output
+import warnings
+warnings.filterwarnings("ignore")
+
+
+# Connecting to Neo4j
+
+def connect_to_neo4j(path):
+    
+    load_dotenv(path)
+
+    uri = os.getenv("NEO4J_URI")
+    user = os.getenv("NEO4J_USER")
+    password = os.getenv("NEO4J_PASSWORD")
+
+    # Creating a Neo4j Driver Instance
+    driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    # Verifying Connection
+    try:
+        driver.verify_connectivity()
+        print('Connected to database successfully.')
+        return driver
+    
+    except Exception as err:
+        print(f"Error: {err}, type: {type(err)}")    
+    
+# Define a function to retrieve data from the knowledge graph based on suitable cypher queries 
+
+def retrieve_data_from_graph(driver, query):
+    
+    result_data = []
+
+    with driver.session() as session:
+        result = session.run(query)
+
+        for record in result:
+            result_data.append(dict(record))
+    
+    return result_data
+
+
+# Function to retrieve all the data for a particular disease
+
+def get_data_of_disease(driver, disease):
+    
+    # Retrieve data for a specific disease from the knowledge graph.
+
+    # Parameters:
+    #     driver: Neo4j driver
+    #     disease_name (str): The name of the disease to retrieve data for.
+
+    # Returns:
+    #     disease_df (pd.DataFrame): DataFrame containing person, disease, biomarker, and protein data.
+    
+    disease_query = """
+    MATCH (p:Person)-[s:SUFFERS_FROM]->(d:Disease)
+    where d.name = '"""+ disease +"""' and s.status IN ['Positive','Negative']
+    RETURN p.respondentId as Respondent_ID,
+    d.name AS DiseaseName,
+    s.status AS DiseaseStatus;
+    """
+
+    disease_df = pd.DataFrame(retrieve_data_from_graph(driver, disease_query))
+    disease_data = disease_df.pivot(index=['Respondent_ID'],columns='DiseaseName', values='DiseaseStatus')
+    disease_data = disease_data.reset_index()
+    disease_data[disease] = disease_data[disease].map({'Negative': 0, 'Positive' : 1})
+
+    biomarker_query = """
+    MATCH (p:Person)-[s:SUFFERS_FROM]->(d:Disease)
+    where d.name = '"""+ disease +"""' and s.status IN ['Positive','Negative']
+    MATCH (p)-[h:HAS]->(b:BloodBiomarker)
+    RETURN p.respondentId as Respondent_ID,
+    p.gender AS Gender,
+    p.age AS Age,
+    p.bmi AS BMI,
+    b.name AS BiomarkerName,
+    b.unit AS BiomarkerUnit,
+    h.value AS BiomarkerValue;
+    """
+
+    biomarker_df = pd.DataFrame(retrieve_data_from_graph(driver, biomarker_query))
+    biomarker_df['BloodBiomarker'] = np.where(biomarker_df['BiomarkerUnit'].notnull(),
+                                       biomarker_df['BiomarkerName'] + ' [' + biomarker_df['BiomarkerUnit'] + ']',
+                                       biomarker_df['BiomarkerName'])
+    biomarker_data = biomarker_df.pivot(index=['Respondent_ID','Gender','Age','BMI'], 
+                                        columns='BloodBiomarker',
+                                        values='BiomarkerValue')
+    biomarker_data = biomarker_data.reset_index()
+    biomarker_data['Gender'] = biomarker_data['Gender'].map({'Female': 0, 'Male' : 1})
+
+
+    protein_query = """
+    MATCH (p:Person)-[s:SUFFERS_FROM]->(d:Disease)
+    where d.name = '"""+ disease +"""' and s.status IN ['Positive','Negative']
+    MATCH (p)-[h:HAS]->(b:BloodProtein)
+    RETURN p.respondentId as Respondent_ID,
+    b.name AS ProteinName,
+    b.unit AS ProteinUnit,
+    h.value AS ProteinValue;
+    """
+
+    protein_df = pd.DataFrame(retrieve_data_from_graph(driver, protein_query))
+    protein_df['BloodProtein'] = np.where(protein_df['ProteinUnit'].notnull(),
+                                       protein_df['ProteinName'] + ' [' + protein_df['ProteinUnit'] + ']',
+                                       protein_df['ProteinName'])
+    protein_data = protein_df.pivot(index=['Respondent_ID'], 
+                                        columns='BloodProtein',
+                                        values='ProteinValue')
+    protein_data = protein_data.reset_index()
+
+    disease_df = biomarker_data.merge(protein_data, on='Respondent_ID',how='inner')
+    disease_df = disease_df.merge(disease_data, on='Respondent_ID',how='inner')
+    
+    return disease_df
+
+
+# Function to preprocess the input dataframe and split it into training and testing sets.
+
+def preprocess_and_split(df, label, impute=True, scale=True, imputer = SimpleImputer(),
+                         scaler = StandardScaler(), test_size=0.2, random_state=42):
+        
+    if 'Respondent_ID' in df.columns:
+        X = df.drop(['Respondent_ID'], axis=1)
+        X = X.drop([label], axis=1)
+    else:    
+        X = df.drop([label], axis=1)
+        
+    y = df[label].copy()
+    
+    # Splitting the data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+    
+    # Separate categorical and numerical columns 
+    categorical_cols = ['Gender'] 
+    numeric_cols = [col for col in X.columns if col not in categorical_cols]
+    
+    # Preprocessing for numerical data
+    numeric_transformer = Pipeline(steps=[
+        ('imputer', imputer) if impute else ('imputer', 'passthrough'),
+        ('scaler', scaler) if scale else ('scaler', 'passthrough')
+    ])
+    
+    # Preprocessing for categorical data
+    categorical_transformer = Pipeline(steps=[
+        ('pass', 'passthrough')])
+    
+    # Column Transformer for numerical and categorical data
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('cat', categorical_transformer, categorical_cols),
+            ('num', numeric_transformer, numeric_cols)
+        ])
+    
+    # Preprocess the data
+    X_train = preprocessor.fit_transform(X_train)
+    X_test = preprocessor.transform(X_test)
+    X = preprocessor.transform(X)
+
+    
+    X_train = pd.DataFrame(X_train, columns= categorical_cols + numeric_cols)
+    X_test = pd.DataFrame(X_test, columns= categorical_cols + numeric_cols)
+    X = pd.DataFrame(X, columns= categorical_cols + numeric_cols)
+
+    return X, y, X_train, X_test, y_train, y_test
+
+
+# Function for model evaluation
+
+def evaluate_model_metrics(model,X_train,y_train,X_test,y_test):
+    
+    y_pred = model.predict(X_test)
+    
+    # Accuracy
+    train_accuracy = accuracy_score(y_train, model.predict(X_train))
+    test_accuracy = accuracy_score(y_test, y_pred)
+    print(f'\nTrain Accuracy: {train_accuracy:.3f}')
+    print(f'\nTest Accuracy: {test_accuracy:.3f}')
+
+    # Precision
+    precision = precision_score(y_test, y_pred)
+    print(f'\nPrecision: {precision:.3f}')
+
+    # Recall
+    recall = recall_score(y_test, y_pred)
+    print(f'\nRecall: {recall:.3f}')
+
+    # F1-score
+    f1 = f1_score(y_test, y_pred)
+    print(f'\nF1-Score: {f1:.3f}')
+
+    # Confusion Matrix
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    print('\nConfusion Matrix:\n')
+    print(conf_matrix)
+
+    # Classification Report
+    class_report = classification_report(y_test, y_pred, target_names=['Class 0', 'Class 1'])
+    print('\n\nClassification Report:\n')
+    print(class_report)
+
+    # ROC Curve and AUC
+    fpr, tpr, thresholds = roc_curve(y_test, y_pred)
+    roc_auc = auc(fpr, tpr)
+
+    print('\nROC-AUC Curve:')
+    plt.figure()
+    plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (area = {roc_auc:.3f})')
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic')
+    plt.legend(loc='lower right')
+    plt.show()
+    
+    
+# Function to perform grid search optimisation
+
+def grid_search_optimize(model, param_grid, X_train, y_train, cv,
+                         scoring='accuracy', verbose=0, n_jobs=-1):
+    
+    grid_search = GridSearchCV(model, param_grid=param_grid, cv=cv, 
+                               scoring=scoring, verbose=verbose, n_jobs=n_jobs)
+    
+    grid_search.fit(X_train, y_train)
+    clear_output()
+    
+    grid_best_params = grid_search.best_params_
+    grid_best_model = grid_search.best_estimator_
+    
+    return grid_best_params, grid_best_model
+
+
+# Function for Random Search
+
+def random_search_optimize(model,param,n_iter,cv,X_train, y_train,
+                           scoring='accuracy', n_jobs=-1, verbose=0):
+    
+    random_search = RandomizedSearchCV(
+        model,
+        param_distributions=param,
+        n_iter=n_iter, 
+        scoring=scoring, 
+        cv=cv, 
+        n_jobs=n_jobs, 
+        verbose=verbose
+    )
+
+    random_search.fit(X_train, y_train)
+    clear_output()
+
+    rand_best_params = random_search.best_params_
+    rand_best_model = random_search.best_estimator_
+    
+    return rand_best_params, rand_best_model
+
+
+# Function for Optuna Hyperparameter Optimization
+
+def optuna_optimize(n_trials=100,direction='maximum',n_jobs=-1):
+    
+    study = optuna.create_study(direction='maximize')  # 'maximize' for accuracy, 'minimize' for loss
+    study.optimize(objective, n_trials=n_trials, n_jobs=n_jobs)
+    clear_output()
+    
+    best_trial = study.best_trial.params
+    log_reg_opt = LogisticRegression(**best_trial)
+    
+    return best_trial, log_reg_opt
+
+
+# K-cross validation
+
+def k_cross_validate(model,X,y,cv,scoring='accuracy',n_jobs=-1):
+    
+    scores = cross_val_score(model, X, y, cv=cv, scoring=scoring)
+    
+    mean_score = scores.mean()
+    std_score = scores.std()
+    
+    print(f"\n{scoring} scores:", scores)
+    print(f"\nMean {scoring} score: ", mean_score)
+    print("\nStandard deviation: ", std_score)
+    
+
+# Stratified K-cross validation
+
+def stratified_k_cross_validate(model,X,y,n_splits,shuffle=True,verbose=0,random_state=42):
+    
+    X = X.values
+    y = y.values
+
+    stratified_kf = StratifiedKFold(n_splits=n_splits, shuffle=shuffle, random_state=random_state)
+
+    scores = []
+
+    # Stratified k-fold cross-validation
+    for train_index, test_index in stratified_kf.split(X, y):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+
+        model.fit(X_train, y_train)
+
+        y_pred = model.predict(X_test)
+
+        accuracy = accuracy_score(y_test, y_pred)
+        precision = precision_score(y_test, y_pred)
+        recall = recall_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred)
+        confusion = confusion_matrix(y_test, y_pred)
+
+        scores.append([accuracy,precision,recall,f1,confusion])
+
+        average_accuracy = np.mean([item[0] for item in scores])
+        average_precision = np.mean([item[1] for item in scores])
+        average_recall = np.mean([item[2] for item in scores])
+        average_f1 = np.mean([item[3] for item in scores])
+
+    for fold, score in enumerate(scores, 1):
+        if verbose >= 1:
+            print(f"Fold {fold}: ")
+            print(f"Accuracy: {score[0]:.3f}")
+        
+        if verbose >= 2:
+            print(f"Precision: {score[1]:.3f}")
+
+        if verbose >= 3:
+            print(f"Recall: {score[2]:.3f}")
+
+        if verbose >= 4:
+            print(f"F1-Score: {score[3]:.3f}")
+
+        if verbose >= 5:
+            print(f"Confusion Matrix:\n {score[4]}")
+            
+        if verbose >= 1:
+            print('')
+
+    print(f"\nAverage Accuracy: {average_accuracy}")
+    print(f"\nAverage Precision: {average_precision}")
+    print(f"\nAverage Recall: {average_recall}")
+    print(f"\nAverage F1-Score: {average_f1}")
+    
+    
+# Feature Selection
+
+# Function to check which features are more correlated with the disease
+
+def get_correlated_features(df,disease,pos,neg):
+    
+    corr_df = df.corr()[disease].sort_values(ascending=False)
+    pos_corr = corr_df[corr_df>pos]
+    neg_corr = corr_df[corr_df<neg]
+    
+    return pos_corr, neg_corr
+
+
+# Function to evaluate the feature set
+
+def evaluate_feature_set(feature_set,df,label,model):
+    
+    new_diabetes_df = df[feature_set+[label]]
+
+    X, y, X_train, X_test, y_train, y_test = preprocess_and_split(df = new_diabetes_df, label = label,
+                                                                  imputer=SimpleImputer(strategy='median'))
+
+    model.fit(X_train, y_train)
+
+    evaluate_model_metrics(log_reg_test,X_train,y_train,X_test,y_test)
+    
+    
+# SelectFromModel can be used with any estimator that exposes feature importance
+
+def select_from_model_features(model,X,y):
+
+    sfm = SelectFromModel(model)  
+
+    
+    sfm.fit_transform(X, y)
+
+    
+    selected_features = sfm.get_support()
+    feature_names = X.columns
+    sfm_features = [feature_names[i] for i, selected in enumerate(selected_features) if selected]
+
+    return sfm_features
+
+
+# Function to select features using SelectKBest using anova scores
+
+def select_k_best(X,y,n):
+    
+    select = SelectKBest(score_func = f_classif, k = n)
+    
+    z = select.fit_transform(X,y)
+
+    feature_rankings = select.scores_
+    feature_rankings_df = pd.DataFrame({'Feature': X.columns, 'Ranking': feature_rankings})
+    sorted_features = feature_rankings_df.sort_values(by='Ranking',ascending=False)
+    
+    return sorted_features.Feature.values
+
+
+#  RFE recursively removes the least important features from the dataset
+
+def select_rfe_features(model,X,y,n,step):
+    
+    rfe = RFE(model,n_features_to_select=n,step=step)
+    fit = rfe.fit(X, y)
+
+    feature_rankings = rfe.ranking_
+    feature_rankings_df = pd.DataFrame({'Feature': X.columns, 'Ranking': feature_rankings})
+    sorted_features = feature_rankings_df.sort_values(by='Ranking')
+    
+    return sorted_features.Feature.values
+
+
+# Uses lasso regularization to give more weights to more important features
+
+def log_reg_lasso_select(X_train,y_train,solver='saga'):
+    
+    l1_reg_model = LogisticRegression(penalty='l1', solver='saga')
+    l1_reg_model.fit(X_train, y_train)
+
+    # Get feature importance coefficients from the model
+    feature_importance = l1_reg_model.coef_[0]
+
+    feature_importance_pairs = [(feature, importance) for feature, importance in zip(X.columns, feature_importance)]
+    feature_importance_pairs.sort(key=lambda x: abs(x[1]), reverse=True)
+
+    l1_sorted_features = [feature for feature, _ in feature_importance_pairs]
+
+    return l1_sorted_features
+
+
+# Relief algorithm focuses on selecting features that are relevant to the target class
+
+def relief_feature_select(n,X,y,n_jobs=-1):
+    
+    fs = ReliefF(n_features_to_select=n, n_jobs=n_jobs)
+    fs.fit(X.values, y.values)
+
+    feature_importances = fs.feature_importances_
+    
+    feature_names = X.columns
+    feature_importance_tuples = list(zip(feature_names, feature_importances))
+    sorted_features = sorted(feature_importance_tuples, key=lambda x: x[1], reverse=True)
+    sorted_feature_names = [feature[0] for feature in sorted_features]
+    relieff_features = sorted_feature_names
+    
+    return relieff_features
+
+
+# Find the optimal number of features for the model
+
+def plot_num_feature_performance(model, df, label, feature_set, cv=10, scoring='accuracy', verbose=False):
+    
+    new_diabetes_df = df[feature_set+[label]]
+
+    X, y, X_train, X_test, y_train, y_test = preprocess_and_split(df = new_diabetes_df, label = label,
+                                                                  impute = True, 
+                                                                  imputer=SimpleImputer(strategy='median'),
+                                                                  random_state=42)
+
+    n_features = X.shape[1]
+        
+    num_features = []
+    accuracies = []
+    num_features_vs_accuracy = {}
+
+    num_features_list = []
+    accuracy_list = []
+
+    # Evaluating the model for each set of number of features
+    for num_features in range(5, X_train.shape[1] + 1):
+
+        X_train_subset = X_train.iloc[:, :num_features]
+        X_test_subset = X_test.iloc[:, :num_features]
+
+        clf = LogisticRegression()
+        clf.fit(X_train_subset, y_train)
+
+        y_pred = clf.predict(X_test_subset)
+    
+        if scoring == 'accuracy':
+            score = accuracy_score(y_test, y_pred)
+        elif scoring == 'precision':
+            score = precision_score(y_test, y_pred)
+        elif scoring == 'recall':
+            score = recall_score(y_test, y_pred)
+        elif scoring == 'f1':
+            score = f1_score(y_test, y_pred)
+
+
+        num_features_list.append(num_features)
+        accuracy_list.append(score)
+        
+        if verbose == True:
+            print(num_features, ':', score)
+        num_features_vs_accuracy[num_features] = score
+    
+    max_score = max(num_features_vs_accuracy.values())
+    max_score_num_feature = [i for i in num_features_vs_accuracy if num_features_vs_accuracy[i]==max_score]
+    print(f'\nMaximum {scoring} = {max_score} for {max_score_num_feature[0]} features')
+
+    plt.figure(figsize=(10,7))
+    plt.plot(num_features_list, accuracy_list, marker='o', linestyle='-')
+    plt.title('Model Performance vs. Number of Features')
+    plt.xlabel('Number of Features')
+    plt.ylabel('Accuracy')
+    plt.show()
+
+
